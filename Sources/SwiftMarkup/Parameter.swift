6d4f543643d60d8816587ee03bfdc44c69592139
Mattt <mattt@me.com>
2021-03-05 04:54:05 -0800

    Add documentation to Parameter

--- Sources/SwiftMarkup/Parameter.swift
+++ Sources/SwiftMarkup/Parameter.swift
@@ -1,7 +1,11 @@
 @_exported import CommonMark
 
+/// A named parameter for a function, initializer, method, or subscript.
 public struct Parameter: Hashable, Codable {
+    /// The name of the parameter.
     public let name: String
+
+    /// The documentation content for the parameter.
     public let content: Document
 
     init(name: String, description: String) throws {
